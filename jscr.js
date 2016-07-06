4e2844dd8fc4fe31fa5f8a2f332ae9e1f98bfa36
umapk <umasara8795@gmail.com>
2016-07-06 09:13:11 +0530

    copy1

---
+++ jscr.js
@@ -0,0 +1,94 @@
+(function()
+{
+}());
+// spin box
+ //  var speed=10,
+ //   movebox = function(moveby) {
+ //       var e1=document.getElementById("box"),
+ //    left=e1.offsetLeft;
+ //    if(( moveby > 0 && left > 400 ) || ( moveby < 0 && left < 51 ))
+ //    {
+ //        clearTimeout(timer);
+ //        timer = setInterval(function() {
+ //        movebox(moveby * -1);
+ //      }, speed);
+ //    }
+ //    e1.style.left = left + moveby + "px";
+ //  };
+ //  var timer = setInterval(function(){movebox(3)}, speed);
+ // }());
+
+
+// MoveBox
+// var speed=30,i=0,
+//
+//     movebox =function()
+//     {
+//     var e1=document.getElementById("box"),
+//     left=e1.offsetLeft,
+//     moveby=1;
+//     e1.style.left=left+moveby+"px";
+//     if(left>400)
+//     {
+//     clearTimeout(timer);
+//     e1.style.top="50px";
+//     e1.style.left="50px";
+//     }
+//     };
+// var timer=setInterval(movebox,speed);
+//
+//
+//
+//
+//
+// }());
+
+
+
+  // var pa1=document.getElementById("pa1");
+  // pa1.className="div1"
+  //
+  // pa1.style.color="red";
+  // pa1.style.border="2px solid black";
+  // pa1.style.padding="2px";
+  //  pa1.style.backgroundColor="yellow";
+ //  var color=window.getComputedStyle("div1",null).getPropertyValue("color");
+ //  alert(color);
+  //  var color=pa1.currentStyle["color"];
+
+
+  //
+  // var getStyle=function(e1,cssProperty)
+  // {
+  //   if(typeof getComputedStyle !== "undefined")
+  //   {
+  //     return window.getComputedStyle(e1,null).getPropertyValue("color");
+  //   }
+  //   else {
+  //     return e1.currentStyle[cssProperty];
+  //   }
+  // };
+  // var color=getStyle(pa1,"color");
+  // alert(color);
+
+
+
+ //  var doc=document,pele=document.querySelectorAll("p");
+ //  alert(pele.length);
+ //  var e1=doc.createElement("p");
+ //  content=doc.createTextNode("<strong>Uma Ammu</strong>");
+ //  pa1=doc.getElementById("p1");
+ //  e1.appendChild(content);
+ // e1.innerHTML="<strong>Uma Ammmu</strong>";
+  // e1.setAttribute("align","center");
+  // doc.body.appendChild(e1);
+  // e1.id="bar";
+  // pa1.parentNode.replaceChild(e1,pa1);
+  // html=e1.innerHTML;
+  // html=html+"<br/> This too";
+  // alert(html);
+  // e1.innerHTML=html;
+  // alert(e1.innerHTML);
+  // var pele=doc.querySelectorAll("p");
+  // alert(pele.length);
+// }());
